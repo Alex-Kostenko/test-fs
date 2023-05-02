cd22267b2e4ad5f400d3efb652fe923d2181619e
Alex Kostenko <0984181760ss@gmail.com>
2023-05-02 17:27:12 +0300

    delete imports

--- client/src/components/Loader/index.test.tsx
+++ client/src/components/Loader/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { useLocation } from "react-router-dom";
 import Loader from ".";
 
